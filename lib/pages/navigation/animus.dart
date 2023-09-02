a19f08c3bc349a969e67a8cf494a58cd9c8902c4
Eshaan Rathi <eshaanrathi2@gmail.com>
2023-09-01 20:15:10 -0700

    feat: - add image_picker and its dependencies. - add app permissions in ios/Runner/PODFILE - add app permissions in ios/Runner/Info.plist
    -future: add permissions for android too

--- lib/pages/navigation/animus.dart
+++ lib/pages/navigation/animus.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-// Old HomePage. Just has logout button.
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class AnimusPage extends StatelessWidget {
-  AnimusPage({super.key});
+class AnimusPage extends StatefulWidget {
+  @override
+  _AnimusPageState createState() => _AnimusPageState();
+}
+
+class _AnimusPageState extends State<AnimusPage> {
+  File? _profileImage;
+  // File? _backgroundImage;
 
   final user = FirebaseAuth.instance.currentUser!;
 
@@ -12,28 +19,103 @@ class AnimusPage extends StatelessWidget {
     FirebaseAuth.instance.signOut();
   }
 
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedImage = await ImagePicker().pickImage(source: source);
+
+    if (pickedImage != null) {
+      final File imageFile = File(pickedImage.path); // Convert XFile to File
+
+      setState(() {
+        if (_profileImage == null) {
+          _profileImage = imageFile;
+        } else {
+          // _backgroundImage = imageFile;
+        }
+      });
+    }
+  }
+
+
+  void _removeImage(bool isProfileImage) {
+    setState(() {
+      if (isProfileImage) {
+        _profileImage = null;
+      } else {
+        // _backgroundImage = null;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.grey, //change your color here
+        appBar: AppBar(
+          iconTheme: const IconThemeData(
+            color: Colors.grey, //change your color here
+          ),
+          actions: [
+            IconButton(
+              onPressed: signUserOut,
+              icon: const Icon(Icons.logout))
+          ],
+          title: const Text(
+            "Animus", 
+            style: TextStyle(
+              color: Colors.black
+            )
+          ),
+          backgroundColor: Colors.white,
         ),
-        actions: [
-          IconButton(
-            onPressed: signUserOut,
-            icon: const Icon(Icons.logout))
-        ],
-        title: const Text(
-          "Animus", 
-          style: TextStyle(
-            color: Colors.black
-          )
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              // Profile Image
+              _profileImage != null
+                  ? Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        CircleAvatar(
+                          radius: 60.0,
+                          backgroundImage: FileImage(_profileImage!),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () => _removeImage(true),
+                        ),
+                      ],
+                    )
+                  : CircleAvatar(
+                      radius: 60.0,
+                      child: IconButton(
+                        icon: Icon(Icons.add_a_photo),
+                        onPressed: () => _pickImage(ImageSource.gallery),
+                      ),
+                    ),
+              SizedBox(height: 20.0),
+              // Background Image
+              // _backgroundImage != null
+              //     ? Stack(
+              //         alignment: Alignment.bottomRight,
+              //         children: [
+              //           Image.file(_backgroundImage!),
+              //           IconButton(
+              //             icon: Icon(Icons.close),
+              //             onPressed: () => _removeImage(false),
+              //           ),
+              //         ],
+              //       )
+              //     : ElevatedButton(
+              //         onPressed: () => _pickImage(ImageSource.gallery),
+              //         child: Text('Upload Background Image'),
+              //       ),
+              SizedBox(height: 20.0),
+              // Other user information widgets can go here
+            ],
+          ),
         ),
-        backgroundColor: Colors.white,
-      ),
-      body: Center(
-        child: Text("Logged in as ${user.email!}"),
       ),
     );
   }
@@ -42,10 +124,22 @@ class AnimusPage extends StatelessWidget {
 
 
 
-// class AnimusPage extends StatelessWidget {
-//   AnimusPage({super.key});
 
-//   // HomePage({super.key});
+
+
+
+
+
+
+// Old HomePage. Just has logout button.
+
+// class AnimusPage extends StatefulWidget {
+//   AnimusPage({super.key});
+//   @override
+//   State<AnimusPage> createState() => _AnimusPageState();
+// }
+
+// class _AnimusPageState extends State<AnimusPage> {
 
 //   final user = FirebaseAuth.instance.currentUser!;
 
@@ -57,14 +151,24 @@ class AnimusPage extends StatelessWidget {
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
+//         iconTheme: const IconThemeData(
+//           color: Colors.grey, //change your color here
+//         ),
 //         actions: [
 //           IconButton(
 //             onPressed: signUserOut,
 //             icon: const Icon(Icons.logout))
 //         ],
+//         title: const Text(
+//           "Animus", 
+//           style: TextStyle(
+//             color: Colors.black
+//           )
+//         ),
+//         backgroundColor: Colors.white,
 //       ),
 //       body: Center(
-//         child: Text("Logged in as " + user.email!),
+//         child: Text("Logged in as ${user.email!}"),
 //       ),
 //     );
 //   }
